d28ed76f8ff4333c0c75e53d525a951d321abcfe
markFalcone <falconemark16@yahoo.com>
2020-02-01 19:11:29 -0800

    implemented progress bar TODO: Complete timer view to display time and stop button on all views

---
+++ EggTimer/TimerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimerViewController.swift
+//  EggTimer
+//
+//  Created by Mark Falcone on 2/1/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
